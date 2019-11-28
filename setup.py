1c32e116dbdca76a3a38c28fc680aa5dba5f7b72
Ricardo Mendes <ricardolsmendes@gmail.com>
2019-11-27 22:25:04 -0300

    Install wheel dependency

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setuptools.setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
